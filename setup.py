b8bc040caeedd6b8fd1455176724a3e651364c09
wachiraericksonkinyua <ericksonkinyua681@gmail.com>
2026-02-17 15:19:39 +0300

    change setup info

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages,setup
 setup(
     name='mcqgenrator',
     version='0.0.1',
-    author='sunny savita',
-    author_email='sunny.savita@ineuron.ai',
+    author='Erickson Wachira',
+    author_email='ericksonkinyua681@gmail.com',
     install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
